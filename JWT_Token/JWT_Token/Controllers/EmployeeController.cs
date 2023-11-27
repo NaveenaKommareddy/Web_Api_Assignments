268a7c4658e2fdaa5c17a866668b9f5f91028f00
NaveenaKommareddy <142799872+NaveenaKommareddy@users.noreply.github.com>
2023-11-27 19:53:06 +0530

    Add files via upload

---
+++ JWT_Token/JWT_Token/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using JWT_Token.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class EmployeeController : ControllerBase
+{
+    [HttpGet]
+    [Route("GetData")]
+    public string GetData()
+    {
+        return "Authenticated with JWT";
+    }
+    [HttpGet]
+    [Route("Details")]
+    public string Details()
+    {
+        return "Authenticated with JWT";
+    }
+
+    [HttpPost]
+    public string AddUser(Users user)
+    {
+        return "User added with username: " + user.Username;
+    }
+}
---
+++ JWT_Token/JWT_Token/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT_Token", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWT_Token");
+    });
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
